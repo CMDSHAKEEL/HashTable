d8b920cdbc9e2c9d093e3966396c1bb8ae0494dc
CMD SHAKEEL <cmdshakeel21@gmail.com>
2021-08-05 20:04:38 +0530

    Added:ability to find frequency of words in large paragraph

--- src/com/bridgelabz/HashTable.java
+++ src/com/bridgelabz/HashTable.java
@@ -1,5 +1,8 @@
 package com.bridgelabz;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class HashTable {
     private HashNode Array[];
     private int noOfArrays;
@@ -12,7 +15,7 @@ public class HashTable {
         this.size = 0;
     }
     public HashTable() {
-        this(10);
+        this(100);
     }
 
     //Create HashNode like Linked list one link and two data types
@@ -69,6 +72,35 @@ public class HashTable {
         }
         return wordCount;
     }
+    //Checking the values of the key
+    public String getKeyValue(Integer key){
+        int arrayIndex = getArrayIndex(key);
+        HashNode head = Array[arrayIndex];
+        while (head != null){
+            if (head.key.equals(key)){
+                return head.value;
+            }
+            head = head.next;
+        }
+        return null;
+    }
+
+
+    //find frequency of words in a large paragraph phrase
+    public void freqOfWords(String str){
+        Map<String, Integer> map = new TreeMap<>();
+        String arr[] = str.split(" ");
+        for (int i=0; i<arr.length; i++){
+            if(map.containsKey(arr[i])){
+                map.put(arr[i], map.get(arr[i])+1);
+            }else {
+                map.put(arr[i], 1);
+            }
+        }
+        for (Map.Entry<String, Integer> entry: map.entrySet()){
+            System.out.println(entry.getKey()+ "--->"+entry.getValue());
+        }
+    }
 
 
     public static void main(String[] args) {
@@ -90,5 +122,11 @@ public class HashTable {
         //Checking size of the HashTable
         System.out.println("The size of hash table : " + hashTable.size);
 
+        //Checking the values of the key
+        System.out.println("Finding Given key value : "+hashTable.getKeyValue(20));
+
+        String paraString = "paranoids are not paranoids because they are paranoid but they keep putting themselves deliberately into paranoid avoidable situation";
+        System.out.println("Given Para String : " + paraString);
+        hashTable.freqOfWords(paraString);
     }
 }
